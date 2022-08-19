3e42177676d8bcd8cefc17ab7f519b1448e75b44
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-08-19 00:14:20 -0700

    Add redirect longurl route to server

--- express_server.js
+++ express_server.js
@@ -40,6 +40,11 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/:${newId}`);
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
