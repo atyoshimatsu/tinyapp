4d09c53e326e7119897f30c265936df509aa0fa6
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-08-19 22:10:09 -0700

    Add error messages to constants

--- constants.js
+++ constants.js
@@ -1,9 +1,14 @@
 const PORT = 8080;
 const ID_LENGTH = 6;
 const CHARACTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+const ERROR_MESSAGES = {
+  '400': 'The Email or password might be empty, or the email is already existing.',
+  '403': 'The Email or password is NOT correct, or the email is NOT existing.',
+}
 
 module.exports = {
   PORT,
   ID_LENGTH,
   CHARACTERS,
+  ERROR_MESSAGES,
 };
